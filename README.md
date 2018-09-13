ae1965e8a522015b95b6e28cee95566cd017badd
CoMatu <30466272+CoMatu@users.noreply.github.com>
2018-09-13 23:21:07 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# coffee
+
+App for coffee houses
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AnimationDemo()
+    );
+  }
+}
+
+class AnimationDemo extends StatelessWidget {
+  const AnimationDemo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const AnimationDemoHome();
+}
