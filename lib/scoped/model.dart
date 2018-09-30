af4798fad4b1ea96d736e9158e57d2df80c12e13
CoMatu <30466272+CoMatu@users.noreply.github.com>
2018-09-30 09:17:42 +0500

    сформирован список заказов

--- lib/scoped/model.dart
+++ lib/scoped/model.dart
@@ -2,9 +2,15 @@ import 'package:coffee/models/product.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class ProductModel extends Model {
+  // Общий список заказа
   List<Product> _orderList = [];
   List<Product> get orderList => _orderList;
 
+  // Список заказа для корзины (название и количество)
+  Map<String, int> _cartList = Map();
+  Map<String, int> get cartList => _cartList;
+
+  // Количество выбранных продуктов для показа в карточке
   int getProductCount(Product product){
     List<Product> proList = [];
     for(int i = 0; i < _orderList.length; i++){
@@ -15,6 +21,7 @@ class ProductModel extends Model {
     return proList.length;
   }
 
+  // Стоимость заказа для показа на FAB
   double getOrderCost(){
     double orderCost = 0.00;
     for(int i = 0; i < _orderList.length; i++){
@@ -24,15 +31,24 @@ class ProductModel extends Model {
     return orderCost;
   }
 
+  // Формируем список уник продуктов для списка в корзине
+
+
 }
 
+// Добавляем продукты в общий список (на карточке продукта)
 abstract class AddProduct extends ProductModel {
   void addProduct(Product product){
     _orderList.add(product);
+
+    // добавляем в карту название-количество если они отсутствуют
+    _cartList.putIfAbsent(product.title, ()=> getProductCount(product));
+
     notifyListeners();
   }
 }
 
+// Убираем продукты из общего списка (на карточке продукта)
 abstract class RemoveProduct extends ProductModel {
   void removeProduct(Product product){
     if(_orderList.length > 0)
